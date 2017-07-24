e557e72a64766ad3e9970d1958fec8ddce0d3fa9
Joe Jordan Brown <joe.jordan.brown@gmail.com>
2017-07-24 16:59:54 +0100

    fix(build): fixed a problem with the UMD build, issue #2

--- build.js
+++ build.js
@@ -63,12 +63,16 @@ return Promise.resolve()
         // The key here is library name, and the value is the the name of the global variable name
         // the window object.
         // See https://github.com/rollup/rollup/wiki/JavaScript-API#globals for more.
-        '@angular/core': 'ng.core'
+        '@angular/core': 'ng.core',
+        '@angular/cdk': 'ng.cdk',
+        '@angular/material': 'ng.material',
       },
       external: [
         // List of dependencies
         // See https://github.com/rollup/rollup/wiki/JavaScript-API#external for more.
-        '@angular/core'
+        '@angular/core',
+        '@angular/cdk',
+        '@angular/material'
       ],
       plugins: [
         commonjs({
