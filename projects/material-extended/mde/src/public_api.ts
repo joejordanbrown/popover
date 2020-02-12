082fa5b435f2992e9bed07f1768433b9f13c1cc7
Joe Jordan Brown <joe.jordan.brown@gmail.com>
2020-02-12 22:59:20 +0000

    update configs

--- projects/material-extended/mde/src/public_api.ts
+++ projects/material-extended/mde/src/public_api.ts
@@ -2,4 +2,9 @@
  * Public API Surface of mde
  */
 
-export * from './lib/popover/index';
+export * from './lib/popover/popover-module';
+export * from './lib/popover/popover';
+export * from './lib/popover/popover-animations';
+export * from './lib/popover/popover-interfaces';
+export * from './lib/popover/popover-trigger';
+export * from './lib/popover/popover-target';
