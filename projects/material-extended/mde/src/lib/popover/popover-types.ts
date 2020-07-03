39aa6f52e7d6d486dd6d7eabdb052472f2688306
Joe <10200431+joejordanbrown@users.noreply.github.com>
2020-07-03 15:55:03 +0100

    feat(popover) scroll strategy (#146)
    * feat(popover): add scroll strategy
    
    * chore: update demo with scroll strategy
    
    * fix(popover): scroll strategy prevent sticking to the viewport

--- projects/material-extended/mde/src/lib/popover/popover-types.ts
+++ projects/material-extended/mde/src/lib/popover/popover-types.ts
@@ -3,3 +3,5 @@ export type MdePopoverPositionX = 'before' | 'after';
 export type MdePopoverPositionY = 'above' | 'below';
 
 export type MdePopoverTriggerEvent = 'click' | 'hover' | 'none';
+
+export type MdePopoverScrollStrategy = 'noop' | 'close' | 'block' | 'reposition';
